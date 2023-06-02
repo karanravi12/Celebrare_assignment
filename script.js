fbacd12deb5865bb1488f2d60a0d3cc476e9a845
Karan Ravi <Karanravi2015@gmail.com>
2023-06-02 11:36:37 +0530

    adding touch

--- script.js
+++ script.js
@@ -5,21 +5,44 @@ let dragOffsetX = 0;
 let dragOffsetY = 0;
 
 draggableText.addEventListener("mousedown", startDrag);
+draggableText.addEventListener("touchstart", startDrag);
+
 document.addEventListener("mousemove", drag);
 document.addEventListener("mouseup", stopDrag);
 
+document.addEventListener("touchmove", drag);
+document.addEventListener("touchend", stopDrag);
+
 function startDrag(event) {
   isDragging = true;
-  const textRect = draggableText.getBoundingClientRect();
-  dragOffsetX = event.clientX - textRect.left;
-  dragOffsetY = event.clientY - textRect.top;
+
+  if (event.type === "mousedown") {
+    dragOffsetX = event.clientX - draggableText.offsetLeft;
+    dragOffsetY = event.clientY - draggableText.offsetTop;
+  } else if (event.type === "touchstart") {
+    const touch = event.touches[0];
+    dragOffsetX = touch.clientX - draggableText.offsetLeft;
+    dragOffsetY = touch.clientY - draggableText.offsetTop;
+  }
 }
 
 function drag(event) {
+  event.preventDefault(); // Prevent scrolling on touch devices
+
   if (isDragging) {
-    const parentRect = draggableText.parentNode.getBoundingClientRect();
-    const x = event.clientX - parentRect.left - dragOffsetX;
-    const y = event.clientY - parentRect.top - dragOffsetY;
+    let clientX, clientY;
+
+    if (event.type === "mousemove") {
+      clientX = event.clientX;
+      clientY = event.clientY;
+    } else if (event.type === "touchmove") {
+      const touch = event.touches[0];
+      clientX = touch.clientX;
+      clientY = touch.clientY;
+    }
+
+    const x = clientX - dragOffsetX;
+    const y = clientY - dragOffsetY;
 
     draggableText.style.left = `${x}px`;
     draggableText.style.top = `${y}px`;
